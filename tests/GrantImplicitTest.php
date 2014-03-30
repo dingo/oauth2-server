58acc19bb8191879cc9faca96c171916a5858f3c
Jason Lewis <jason.lewis1991@gmail.com>
2014-03-31 00:31:29 +1100

    Test coverage for implicit grant.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/GrantImplicitTest.php
+++ tests/GrantImplicitTest.php
@@ -2,9 +2,35 @@
 
 use Mockery as m;
 use Dingo\OAuth2\Grant\Implicit;
+use Dingo\OAuth2\Entity\Token as TokenEntity;
 use Symfony\Component\HttpFoundation\Request;
 
 class GrantImplicitTest extends PHPUnit_Framework_TestCase {
+
+
+
+	public function testHandlingAuthorizationRequestReturnsTokenEntity()
+	{
+		$grant = (new Implicit)->setStorage($storage = $this->getStorageMock());
+
+		// Set up the expectations so that when the create method is
+		// called an AuthorizationCode entity is returned.
+		$storage->shouldReceive('get')->with('token')->andReturn(m::mock([
+			'create' => new TokenEntity('test', 'access', 'test', 1, $expires = time() + 120),
+			'associateScopes' => true
+		]));
+
+		$token = $grant->handleAuthorizationRequest('test', 1, 'test', []);
+
+		$this->assertEquals([
+			'token' => 'test',
+			'type' => 'access',
+			'client_id' => 'test',
+			'user_id' => 1,
+			'expires' => $expires,
+			'scopes' => []
+		], $token->getAttributes());
+	}
 
 
 	public function testCorrectGrantIdentifier()
